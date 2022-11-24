b35ecbef4c8015c1254f72ad3d627f7d6cdb023c
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-11-24 19:16:17 +0530

    hi

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
 from flask import request
 from flask import Flask
+import string
 from flask import jsonify
 # import numpy as np
 # import string
 # from nltk.corpus import stopwords
-# import pandas as pd
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.feature_extraction.text import TfidfTransformer,TfidfVectorizer
-# from sklearn.pipeline import Pipeline
+# import pandas as pd 
 # # import pickle
-import pickle
+import gzip,pickle
+def cleaner(x):
+    return [a for a in (''.join([a for a in x if a not in string.punctuation])).lower().split()]
 
 # test for ML
 
 app = Flask(__name__)
 
 
+ 
+# with open('./model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+# model=pickle.load(open('model.pkl', 'rb'))
+
+class CustomUnpickler(pickle.Unpickler):
+
+    def find_class(self, module, name):
+        try:
+            return super().find_class(__name__, name)
+        except AttributeError:
+            return super().find_class(module, name)
+
+model = CustomUnpickler(open('model.pkl', 'rb')).load()
+
 @app.route('/', methods=['GET'])
-def index():
-    # global model
-    # model = pickle.load(open('model.pkl', 'rb'))
+def index():       
     return "<h1>Vahini Chatbot API</h1>"
 
 
 @app.route("/ml/reply", methods=['GET'])
 def reply():
-    # ques = request.args.get('question', default="hello", type=str)
-    # ques = [ques]
-    # ans = model.predict(ques)[0]
-    # return jsonify(reply=ans)
-    return "< h1 > hi < /h1 >"
+    ques = request.args.get('question', default="hello", type=str)
+    ques = [ques]
+    ans = model.predict(ques)[0]
+    return jsonify(reply=ans)
+    # return "< h1 > hi < /h1 >"
---
+++ try.py
@@ -0,0 +1,2 @@
+import pickle
+model = pickle.load(open('./model.pkl', 'rb'))
